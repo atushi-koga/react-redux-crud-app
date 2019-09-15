3ab8cad1fbe4cc142f15ba4d9b05cd6ee89a4ed3
atsushi.koga <stbe51@gmail.com>
2019-09-15 13:48:02 +0900

    use props, prop-types

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
+  const profiles = [
+    {name: "Tanaka", age: 10},
+    {name: "Satou", age: 5},
+    {name: "Takahashi", age: 1}
+  ];
   return (
       <div>
-        <Cat/>
-        <Cat/>
-        <Cat/>
+        {
+          profiles.map((profile, i) => <User name={profile.name} age={profile.age} key={i}/>)
+        }
       </div>
   )
 };
 
-const Cat = () => {
+const User = (props) => {
   return (
-      <div>Meow!</div>
+      <div>My name is {props.name} and {props.age} years old</div>
   );
 };
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
+};
+
+// User.defaultProps = {
+//   age: 1
+// };
 
 export default App;

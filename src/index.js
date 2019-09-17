b87c8c5b4933df72acf030acaca8204f6ded18b4
atsushi.koga <stbe51@gmail.com>
2019-09-17 21:53:46 +0900

    CRUD App: create new event

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
 import reducer from './reducers';
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import thunk from "redux-thunk";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 // storeの中にthunkミドルウェアを組み込み、storeを作成する
 // action creatorが関数を返す事ができるようになる
@@ -14,7 +16,12 @@ const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
-        <EventsIndex/>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/events/new" component={EventsNew}/>
+                <Route exact path="/" component={EventsIndex}/>
+            </Switch>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 // 全Reducerを1つに結合する
 
 import {combineReducers} from "redux";
+import {reducer as form} from 'redux-form';
 import events from './events';
 
-export default combineReducers({events});
+export default combineReducers({events, form});

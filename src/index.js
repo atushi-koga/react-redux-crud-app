64cd3e886afb3902f52c0184c0167d064f5c9770
atsushi.koga <stbe51@gmail.com>
2019-09-18 08:55:39 +0900

    CRUD App: apply redux-dev-tools

--- src/index.js
+++ src/index.js
@@ -9,10 +9,13 @@ import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 import thunk from "redux-thunk";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {composeWithDevTools} from 'redux-devtools-extension';
 
+const enhancer = process.env.NODE_ENV === 'development' ?
+    composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk);
 // storeの中にthunkミドルウェアを組み込み、storeを作成する
 // action creatorが関数を返す事ができるようになる
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
     <Provider store={store}>

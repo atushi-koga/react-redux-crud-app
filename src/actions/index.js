61eaa6f1ae188eea6fa7c16a503313a571830b7d
atsushi.koga <stbe51@gmail.com>
2019-09-21 11:21:09 +0900

    CRUD App: update event

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 
 // Actions
+export const READ_EVENT = 'READ_EVENT';
 export const READ_EVENTS = 'READ_EVENTS';
 export const CREATE_EVENT = 'CREATE_EVENT';
 export const DELETE_EVENT = 'DELETE_EVENT';
+export const UPDATE_EVENT = 'UPDATE_EVENT';
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1';
 const QUERY_STRING = '?token=token123';
 
 // Action Creators
+// dispatchメソッドの第一引数であるオブジェクトは、Reducersの第二引数のactionに渡される
 export const readEvents = () => async dispatch => {
     const response = await axios.get(`${ROOT_URL}/events${QUERY_STRING}`);
     dispatch({type: READ_EVENTS, response});
@@ -17,6 +20,16 @@ export const readEvents = () => async dispatch => {
 export const postEvent = values => async dispatch => {
     const response = await axios.post(`${ROOT_URL}/events${QUERY_STRING}`, values);
     dispatch({type: CREATE_EVENT, response});
+};
+
+export const getEvent = id => async dispatch => {
+    const response = await axios.get(`${ROOT_URL}/events/${id}${QUERY_STRING}`);
+    dispatch({type: READ_EVENT, response});
+};
+
+export const putEvent = values => async dispatch => {
+    const response = await axios.put(`${ROOT_URL}/events/${values.id}${QUERY_STRING}`, values);
+    dispatch({type: UPDATE_EVENT, response});
 };
 
 export const deleteEvent = id => async dispatch => {
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
-import {DELETE_EVENT, READ_EVENTS} from "../actions";
+import {
+    CREATE_EVENT,
+    READ_EVENTS,
+    READ_EVENT,
+    UPDATE_EVENT,
+    DELETE_EVENT,
+} from "../actions";
 
 // Reducers
 // actionとstateから新たなstateを作成し、返す
+// events: メモリに保持しているイベントの一覧
 export default (events = {}, action) => {
     switch (action.type) {
+        case CREATE_EVENT:
+        case READ_EVENT:
+        case UPDATE_EVENT:
+            const data = action.response.data;
+
+            return {...events, [data.id]: data};
+
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id');
 
